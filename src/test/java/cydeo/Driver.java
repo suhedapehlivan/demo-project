f5efd736494c00a82afc69cb94b9690850ef71f8
suhedapehlivan <suheda.90@gmail.com>
2023-03-06 17:40:25 +0300

    search tas is added

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "3.94.153.85";
+                        String gridAddress = "44.214.95.236";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
